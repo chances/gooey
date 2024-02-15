ad7fc9c887b0cea0a7ae46044223b977d6e0c91a
Chance Snow <git@chancesnow.me>
2024-02-15 17:13:32 -0600

    Stub out a GPU rasterizer class

---
+++ source/gooey/rasterization/gpu.d
@@ -0,0 +1,9 @@
+/// Authors: Chance Snow
+/// Copyright: Copyright © 2022 Chance Snow. All rights reserved.
+/// License: MIT License
+module gooey.rasterization.gpu;
+
+import gooey.rasterization : Rasterizer;
+
+///
+class GpuRasterizer : Rasterizer {}
